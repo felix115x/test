ac95054d1ebe96cdd4f0656fb0e93791e3a5c64e
Grinvalds, Edgars <edgars.grinvalds@seb.se>
2019-07-17 11:33:44 +0300

    good check

---
+++ src/main/java/teamcalypso/Iterator.java
@@ -0,0 +1,17 @@
+package teamcalypso;
+
+public class Iterator<E extends Storage> {
+    public Iterator(E[] data) {
+        //TODO implement method body
+    }
+
+    public boolean hasNext() {
+        //TODO implement method body
+        return false;
+    }
+
+    public E next() {
+        //TODO implement method body
+        return null;
+    }
+}
---
+++ src/main/java/teamcalypso/Sorter.java
@@ -0,0 +1,14 @@
+package teamcalypso;
+
+public class Sorter<E extends Storage> {
+    //TODO implement logic for this class to sort items by count descending and type ascending (in case some of items is with the same count)
+
+    public Sorter(E[] data) {
+        //TODO implement method body
+    }
+
+    public E[] sort() {
+        //TODO implement method body
+        return null;
+    }
+}
